fc0aaab510e0c1473370daab6535749e6a8e962d
Ronald De Mesa <ronaldreyjr061201@gmail.com>
2025-11-01 01:21:40 +0800

    add header links for pages

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,26 @@
 import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
 
 function Header() {
   return (
     <header className="py-3">
       <ul className="type-list-none flex flex-row gap-10 items-center justify-center text-xl">
-        <li>Home</li>
-        <li>Features</li>
-        <li>Sign in</li>
+        <li>
+          <Button variant="link" size={"lg"} asChild>
+            <Link href={"/"}>Home</Link>
+          </Button>
+        </li>
+        <li>
+          <Button variant="link" size={"lg"} asChild>
+            <Link href={"/features"}>Features</Link>
+          </Button>
+        </li>
+        <li>
+          <Button variant="link" size={"lg"} asChild>
+            <Link href={"/login"}>Sign in</Link>
+          </Button>
+        </li>
       </ul>
     </header>
   );
